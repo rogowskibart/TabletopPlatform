6ac975e321a4d0885a799c9bf7ecabe973cae7e7
Bartosz Rogowski <rogowskibart@gmail.com>
2021-05-26 17:47:43 +0200

    Initial commit

---
+++ src/main/java/com/example/tabletopplatform/TabletopPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.example.tabletopplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TabletopPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TabletopPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tabletopplatform/TabletopPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tabletopplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TabletopPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
