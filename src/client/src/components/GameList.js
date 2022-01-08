554ef73d6b9bdf8067794365a876cd0ce81fd88f
Bartosz Rogowski <rogowskibart@gmail.com>
2022-01-08 16:17:19 +0100

    Make onButtonClick set state for game list

--- src/client/src/components/GameList.js
+++ src/client/src/components/GameList.js
@@ -2,12 +2,27 @@ import React from 'react';
 import axios from 'axios';
 
 class GameList extends React.Component {
-  constructor() {
-    axios.get('http://localhost:8080/api/v1/games', {});
-  }
+  state = { gameList: [] };
+
+  onButtonClick = async () => {
+    const response = await axios.get('http://localhost:8080/api/v1/games/', {
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+      },
+    });
+
+    this.setState({ gameList: response.data.games });
+  };
 
   render() {
-    return <div>GameList</div>;
+    return (
+      <div>
+        <button class="ui button" onClick={this.onButtonClick}>
+          Click for games
+        </button>
+        <div>Found {this.state.gameList.length} games</div>
+      </div>
+    );
   }
 }
 
